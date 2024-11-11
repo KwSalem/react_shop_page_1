06cf51eb190fa370cb43a87207991bb3a06ae06e
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-11-11 20:02:48 +0300

    Done task4

--- src/App.js
+++ src/App.js
@@ -1,46 +1,8 @@
 import "./App.css";
 import shop from "./images/image.png";
+import productsList from "./Components/products";
 
 function App() {
-  // JavaScript is Below
-  // products arrays
-  const products = [
-    {
-      name: "Chocolate Chip Cookies",
-      price: 10,
-      image:
-        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F1711348559552%2FUntitled_design_7_.png",
-    },
-    {
-      name: "Cute Cookies",
-      price: 10,
-      image:
-        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F1714371033624%2Fpinach_pumpkin_puree_12_.png",
-    },
-    {
-      name: "Cute Cookies",
-      price: 30,
-      image:
-        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F20240502072702%2FUntitled_design_19_.png",
-    },
-    {
-      name: "Cute Cookies",
-      price: 50,
-      image:
-        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F1711352923728%2FUntitled_design_23_.png",
-    },
-  ];
-
-  const productsList = products.map((product, index) => {
-    return (
-      /*display Pictures and Names' products and Prices  */
-      <div key={index} className="Product-Card">
-        <img src={product.image} alt={product.name} className="Product-Image" />
-        <h2>{product.name}</h2>
-        <p>{product.price} KD</p>
-      </div>
-    );
-  });
   // JSX Codes is Below
   return (
     <div className="App">

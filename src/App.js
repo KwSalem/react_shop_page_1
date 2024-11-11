5ebd986f5ce7cae968ddecb175d9cceccb4c406a
KwSalem <167813808+KwSalem@users.noreply.github.com>
2024-11-11 18:52:49 +0300

    Done Task3

--- src/App.js
+++ src/App.js
@@ -17,6 +17,18 @@ function App() {
       image:
         "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F1714371033624%2Fpinach_pumpkin_puree_12_.png",
     },
+    {
+      name: "Cute Cookies",
+      price: 30,
+      image:
+        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F20240502072702%2FUntitled_design_19_.png",
+    },
+    {
+      name: "Cute Cookies",
+      price: 50,
+      image:
+        "https://d1lgpy5kbjv9mr.cloudfront.net/public%2Frecipe%2Fthumbnail%2F1711352923728%2FUntitled_design_23_.png",
+    },
   ];
 
   const productsList = products.map((product, index) => {
